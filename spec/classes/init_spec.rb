95566ce0ec58f3f642f8fae906c0f61be1fabaa5
Chris Tessmer <chris.tessmer@onyxpoint.com>
2016-05-13 22:13:54 -0400

    (SIMP-1054) Update static assets to fix spec tests
    Before this patch, this module would fail its spec tests when
    `STRICT_VARIABLES=yes` is set (as it is in Travis CI tests).
    This was caused by the presence of the unset (and unused) variable
    "spec_title" in the modules' fixture hieradata/hiera.yaml.
    
    This commit fixes the issue by removing "spec_title" from
    each module's hieradata/hiera.yaml.
    
    SIMP-1054 #comment Fixed pupmod-simp-postfix
    SIMP-1090 #close Fixed spec failures when `STRICT_VARIABLES=yes`
    
    Change-Id: I550b54280c6ea6f6bff3916533e781fd601ea131

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,30 +1,29 @@
 require 'spec_helper'
 
 describe 'postfix' do
-  it { is_expected.to create_class('postfix') }
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) { facts }
+      it { is_expected.to create_class('postfix') }
 
-  base_facts = {
-    :operatingsystem => 'RedHat',
-    :grub_version => '2.0',
-    :uid_min => '500'
-  }
-  let(:facts){base_facts}
 
-  context 'base' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to contain_simp_file_line('/root/.bashrc') }
-    it { is_expected.to contain_exec('postalias').that_requires('Package[postfix]') }
-    it { is_expected.to contain_exec('postalias').that_subscribes_to('File[/etc/aliases]') }
-    it { is_expected.to contain_file('/etc/aliases').that_subscribes_to('Concat_build[postfix]') }
-    it { is_expected.to contain_package('postfix') }
-    it { is_expected.to contain_package('mutt') }
-    it { is_expected.to contain_user('postfix') }
-    it { is_expected.to contain_file('/root/.muttrc').with({ 'replace' => false }) }
-  end
+      context 'base' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_simp_file_line('/root/.bashrc') }
+        it { is_expected.to contain_exec('postalias').that_requires('Package[postfix]') }
+        it { is_expected.to contain_exec('postalias').that_subscribes_to('File[/etc/aliases]') }
+        it { is_expected.to contain_file('/etc/aliases').that_subscribes_to('Concat_build[postfix]') }
+        it { is_expected.to contain_package('postfix') }
+        it { is_expected.to contain_package('mutt') }
+        it { is_expected.to contain_user('postfix') }
+        it { is_expected.to contain_file('/root/.muttrc').with({ 'replace' => false }) }
+      end
 
-  context 'with_server_enabled' do
-    let(:params) {{ :enable_server => true }}
-    it {is_expected.to create_class('postfix::server') }
-    it { is_expected.to compile.with_all_deps }
+      context 'with_server_enabled' do
+        let(:params) {{ :enable_server => true }}
+        it {is_expected.to create_class('postfix::server') }
+        it { is_expected.to compile.with_all_deps }
+      end
+    end
   end
 end
--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -1,110 +1,108 @@
 require 'spec_helper'
 
 describe 'postfix::server' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      let(:facts) { facts }
 
-  base_facts = {
-    :operatingsystem => 'RedHat',
-    :grub_version => '2.0',
-    :uid_min => '500'
-  }
-  let(:facts){base_facts}
+      it { is_expected.to create_class('postfix') }
+      it { is_expected.to create_class('postfix::server') }
 
-  it { is_expected.to create_class('postfix') }
-  it { is_expected.to create_class('postfix::server') }
+      context 'base' do
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('iptables') }
+        it { is_expected.to create_class('pki') }
+        it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix').with
+          ({
+            'dports' => ['25','587']
+          })
+        }
+        it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
 
-  context 'base' do
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to create_class('iptables') }
-    it { is_expected.to create_class('pki') }
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix').with
-      ({
-        'dports' => ['25','587']
-      })
-    }
-    it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
-  end
+      context 'mandatory_cipher_validation' do
+        err = 'bad stuff'
+        let(:params){{ :mandatory_ciphers => err }}
+        it {
+          expect {
+            is_expected.to compile.with_all_deps
+          }.to raise_error(/does not contain '#{err}'/)
+        }
+      end
 
-  context 'mandatory_cipher_validation' do
-    err = 'bad stuff'
-    let(:params){{ :mandatory_ciphers => err }}
-    it {
-      expect {
-        is_expected.to compile.with_all_deps
-      }.to raise_error(/does not contain '#{err}'/)
-    }
-  end
+      context 'just_localhost' do
+        let(:params){{ :inet_interfaces => ['localhost'] }}
 
-  context 'just_localhost' do
-    let(:params){{ :inet_interfaces => ['localhost'] }}
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.not_to create_class('iptables') }
+        it { is_expected.not_to create_class('pki') }
+        it { is_expected.not_to create_iptables__add_tcp_stateful_listen('allow_postfix') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.not_to create_class('iptables') }
-    it { is_expected.not_to create_class('pki') }
-    it { is_expected.not_to create_iptables__add_tcp_stateful_listen('allow_postfix') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
-  end
+      context 'no_iptables' do
+        let(:params){{ :enable_iptables => false }}
 
-  context 'no_iptables' do
-    let(:params){{ :enable_iptables => false }}
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.not_to create_class('iptables') }
+        it { is_expected.to create_class('pki') }
+        it { is_expected.not_to create_iptables__add_tcp_stateful_listen('allow_postfix') }
+        it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.not_to create_class('iptables') }
-    it { is_expected.to create_class('pki') }
-    it { is_expected.not_to create_iptables__add_tcp_stateful_listen('allow_postfix') }
-    it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
-  end
+      context 'no_enable_user_connect' do
+        let(:params){{ :enable_user_connect => false }}
 
-  context 'no_enable_user_connect' do
-    let(:params){{ :enable_user_connect => false }}
+        it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix').with({ 'dports' => '25' }) }
+      end
 
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix').with({ 'dports' => '25' }) }
-  end
+      context 'no_enable_tls' do
+        let(:params){{ :enable_tls => false }}
 
-  context 'no_enable_tls' do
-    let(:params){{ :enable_tls => false }}
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('iptables') }
+        it { is_expected.not_to create_class('pki') }
+        it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to create_class('iptables') }
-    it { is_expected.not_to create_class('pki') }
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
-  end
+      context 'no_enforce_tls' do
+        let(:params){{ :enforce_tls => false }}
 
-  context 'no_enforce_tls' do
-    let(:params){{ :enforce_tls => false }}
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('iptables') }
+        it { is_expected.to create_class('pki') }
+        it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
+        it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
 
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to create_class('iptables') }
-    it { is_expected.to create_class('pki') }
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
-    it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.not_to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
-  end
+      context 'no_enable_simp_pki' do
+        let(:params){{ :enable_simp_pki => false }}
 
-  context 'no_enable_simp_pki' do
-    let(:params){{ :enable_simp_pki => false }}
-
-    it { is_expected.to compile.with_all_deps }
-    it { is_expected.to create_class('iptables') }
-    it { is_expected.not_to create_class('pki') }
-    it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
-    it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
-    it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
-    it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to create_class('iptables') }
+        it { is_expected.not_to create_class('pki') }
+        it { is_expected.to create_iptables__add_tcp_stateful_listen('allow_postfix') }
+        it { is_expected.to create_postfix_main_cf('smtp_use_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_enforce_tls') }
+        it { is_expected.to create_postfix_main_cf('smtp_tls_mandatory_ciphers').with({ 'value' => 'high' }) }
+        it { is_expected.not_to contain_pki__copy('/etc/postfix').that_notifies('Service[postfix]') }
+      end
+    end
   end
 end
