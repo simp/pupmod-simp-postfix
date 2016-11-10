4e90a89ff42dc9756a883ac25a27bf9e6a3dd541
Liz Nemsick <lnemsick.simp@gmail.com>
2016-11-10 11:45:44 -0500

    (SIMP-1677) Remove deprecated Puppet.newtype
    Eliminated use of deprecated Puppet.newtype
    
    SIMP-1899 #close

--- lib/puppet/type/postfix_main_cf.rb
+++ lib/puppet/type/postfix_main_cf.rb
@@ -1,37 +1,35 @@
-module Puppet
-  newtype(:postfix_main_cf) do
-    require 'puppet/util/selinux'
-    include Puppet::Util::SELinux
+Puppet::Type.newtype(:postfix_main_cf) do
+  require 'puppet/util/selinux'
+  include Puppet::Util::SELinux
 
-    @doc = "Modifies settings in the postfix main.cf configuration file."
+  @doc = "Modifies settings in the postfix main.cf configuration file."
 
-    def initialize(args)
-      super(args)
+  def initialize(args)
+    super(args)
 
-      if self[:notify] then
-        self[:notify] += ['Service[postfix]']
-      else
-        self[:notify] = ['Service[postfix]']
-      end
+    if self[:notify] then
+      self[:notify] += ['Service[postfix]']
+    else
+      self[:notify] = ['Service[postfix]']
     end
-    newparam(:name) do
-      isnamevar
-      desc "The parameter to modify."
+  end
+  newparam(:name) do
+    isnamevar
+    desc "The parameter to modify."
+  end
+
+  newproperty(:value) do
+    desc "The value to which to set the named parameter."
+  end
+
+  validate do
+    must_supply = [:value]
+
+    found = true
+    must_supply.each do |var|
+      not self[var] or self[var].empty? and found = false and break
     end
 
-    newproperty(:value) do
-      desc "The value to which to set the named parameter."
-    end
-
-    validate do
-      must_supply = [:value]
-
-      found = true
-      must_supply.each do |var|
-        not self[var] or self[var].empty? and found = false and break
-      end
-
-      raise(ArgumentError,"You must supply all of '#{must_supply.join(', ')}'") if not found
-    end
+    raise(ArgumentError,"You must supply all of '#{must_supply.join(', ')}'") if not found
   end
 end
